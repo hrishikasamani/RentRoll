bd86d5eb17c1e017c8899585d3a016c3a9c982bf
hrishikasamani <samani.hr@northeastern.edu>
2024-11-08 02:46:37 -0500

    changes

--- Client/src/App.js
+++ Client/src/App.js
@@ -35,7 +35,7 @@ function App() {
              
             }
           ></Route>
-          <Route path="/login" exact element={<Login />}></Route>
+          <Route path="/login" exact element={<Login />}/>
           <Route path="/register" exact element={<Register />}>
             {" "}
           </Route>
